8b5e3d9ce9cca14c32352e755276bb028dadd1ce
kevincisnero01 <kevingabriel18@gmail.com>
2023-09-29 11:13:36 -0400

    test(general): finish version 1

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,9 +1,9 @@
 <x-main-layout>
     <!-- ===== HERO SECTION===== -->
-    <div class="relative overflow-hidden bg-gray-800">
+    <div class="relative overflow-hidden bg-[#3d4249]">
         <div class="max-w-7xl mx-auto">
-            <div class="relative z-10 pb-8 bg-gray-800 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-                <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-gray-800 transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+            <div class="relative z-10 pb-8 bg-[#3d4249] sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-[#3d4249] transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
                     <polygon points="50,0 100,0 50,100 0,100"></polygon>
                 </svg>
 
@@ -29,7 +29,7 @@
     </div>
 
     <!-- ===== FOOTER SECTION===== -->
-    <section class="bg-gray-800">
+    <section class="bg-[#3d4249]">
         <div class="flex justify-center pt-10">
             <h2 class="text-2xl font-extrabold text-gray-200">{{ __('Contact me') }}</h2>
         </div>
